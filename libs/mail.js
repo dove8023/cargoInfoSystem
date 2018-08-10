9f40be8cb7d4317402bfec5382e2511e2295646b
Mr.He <hexisen@tik.com>
2018-08-10 14:29:32 +0800

    add mail

--- libs/mail.js
+++ libs/mail.js
@@ -1,8 +1,8 @@
 /*
  * @Author: he@whaleblue.design 
  * @Date: 2018-08-09 22:24:15 
- * @Last Modified by: he@whaleblue.design
- * @Last Modified time: 2018-08-09 23:12:53
+ * @Last Modified by: Mr.He
+ * @Last Modified time: 2018-08-10 14:16:11
  * @content what is the content of this file. */
 
 
@@ -21,8 +21,8 @@ let mailConfig = {
         // user: 'vivianmaple',
         // pass: 'Tik123456'
 
-        user: 'test@goodsinfo.cn',
-        pass: 'Hexisen123'
+        user: 'service@goodsinfo.cn',
+        pass: 'Tap4fun99'
 
         // user: 'tik@zuimd.com',
         // pass: 'wang.chao.507'
@@ -38,13 +38,13 @@ const sendMail = () => {
     return new Promise((resolve, reject) => {
         transporter.sendMail({
             // from: 'service@tik.com',
-            from: 'test@goodsinfo.cn',
+            // from: 'test@goodsinfo.cn',
             // from: 'tik@goodsinfo.cn',
             // from: `<foo@example.com>`,
-            // from: "tik@goodsinfo.cn",
-            to: 'hexisen1991@gmail.com',
-            subject: 'subject',
-            html: 'hello html 22222',
+            from: "service@tik.com          <service@goodsinfo.cn>",
+            to: 'hexisen@tik.com',
+            subject: '欢迎登陆 ',
+            html: '<p>您好</p>',
         }, function (err, _) {
             if (err) {
                 console.log('1111Error: ' + err)
