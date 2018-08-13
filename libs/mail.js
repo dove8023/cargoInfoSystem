11e55dba400518c9111fb0afbc228e22157310b8
Mr.He <hexisen@tik.com>
2018-08-13 10:51:22 +0800

    邮箱设置

--- libs/mail.js
+++ libs/mail.js
@@ -2,30 +2,29 @@
  * @Author: he@whaleblue.design 
  * @Date: 2018-08-09 22:24:15 
  * @Last Modified by: Mr.He
- * @Last Modified time: 2018-08-10 19:56:30
+ * @Last Modified time: 2018-08-13 10:50:39
  * @content what is the content of this file. */
 
 
 const nodemailer = require('nodemailer')
 
 let mailConfig = {
-    // host: 'mx.hox.com',
-    // host: 'smtp.qq.com',
-    // service: 'qq',
-    // host: 'smtp.exmail.qq.com',
-    host: 'smtp.mxhichina.com',
-    secureConnection: false,
-    use_authentication: false,
+    host: 'smtp.exmail.qq.com',
+    secureConnection: true,
+    use_authentication: true,
     port: 465,
     auth: {
         // user: 'vivianmaple',
         // pass: 'Tik123456'
 
-        user: 'service@goodsinfo.cn',
-        pass: 'Tap4fun99'
+        // user: 'service@goodsinfo.cn',
+        // pass: 'Tap4fun99'
 
         // user: 'tik@zuimd.com',
         // pass: 'wang.chao.507'
+
+        user: 'service@tik-support.com',
+        pass: 'Tap4fun99123'
     }
 }
 
@@ -41,8 +40,8 @@ const sendMail = () => {
             // from: 'test@goodsinfo.cn',
             // from: 'tik@goodsinfo.cn',
             // from: `<foo@example.com>`,
-            from: "service@tik.com          <service@goodsinfo.cn>",
-            to: 'hexisen8023@sina.com',
+            from: "service@tik.com <service@tik-support.com>",
+            to: 'hexisen@tik.com',
             subject: '欢迎登陆 ',
             html: '<p>您好</p>',
         }, function (err, _) {
