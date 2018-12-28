5d41a8804900586701bb419c7510f1b968d8f1cb
Mr.He <hexisen@tik.com>
2018-12-28 10:58:06 +0800

    fix: drop

--- test.js
+++ test.js
@@ -1,9 +1,46 @@
-const http = require("http");
+let request = require("request-promise");
+
+let success = 0, fail = 0, lost = 0;
+
+async function sendMail(i) {
+
+    let options = {
+        uri: 'http://172.20.132.41:3000/v1/email',
+        // uri: 'http://localhost:3000/v1/email',
+        method: 'post',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: {
+            email: "hexisen@tik.com",
+            title: "TEST",
+            appId: "1101",
+            content: "SSSFFFFFFFFFF"
+        },
+        json: true
+    };
+
+    try {
+        let result = await request(options);
+        if (result.code == 0) {
+            success++;
+        } else {
+            fail++;
+        }
+
+        console.log(i, result)
+    } catch (e) {
+        console.log(e);
+        lost++;
+    }
+
+}
 
 
-http.createServer((req, res) => {
-    console.log("yes");
-    res.end("ok");
-}).listen(3000, () => {
-    console.log("http running");
-})
+for (let i = 0; i < 4400; i++) {
+    sendMail(i);
+}
+
+setTimeout(() => {
+    console.log("success : ", success, " fail : ", fail, " lost: ", lost)
+}, 20000);
